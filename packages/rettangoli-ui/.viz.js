fd079ff15f99bee5d1bc0da34fc894c50b1dd6e7
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2024-08-16 11:56:03 +0800

    test supoprt multiple screen sizes

--- packages/rettangoli-ui/.viz.js
+++ packages/rettangoli-ui/.viz.js
@@ -1,11 +1,19 @@
 
-
 const config = () => {
     return {
         dir: {
             input: "pages/specs",
             output: "tests",
         },
+        screenSizes: [{
+            name: "mobile",
+            width: 375,
+            height: 667,
+        }, {
+            name: "desktop",
+            width: 1024,
+            height: 768,
+        }]
     };
 }
 
