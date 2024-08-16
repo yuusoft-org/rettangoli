7b4104538cd68c9772c79095d51a90158116a1d9
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2024-08-17 00:17:56 +0800

    remove form

--- packages/rettangoli-ui/src/index.js
+++ packages/rettangoli-ui/src/index.js
@@ -2,12 +2,10 @@ import RettangoliButton from './components/button.js'
 import RettangoliView from './components/view.js';
 import RettangoliText from './components/text.js';
 import RettangoliImage from './components/image.js';
-import RettangoliForm from './components/form.js';
 import RettangoliSvg from './components/svg.js';
 
 customElements.define("rtgl-button", RettangoliButton);
 customElements.define("rtgl-view", RettangoliView);
 customElements.define("rtgl-text", RettangoliText);
 customElements.define("rtgl-image", RettangoliImage);
-customElements.define("rtgl-form", RettangoliForm);
 customElements.define("rtgl-svg", RettangoliSvg);
