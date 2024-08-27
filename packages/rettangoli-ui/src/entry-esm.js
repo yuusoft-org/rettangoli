b12bae28e59b604d86022adf187fbcc9a198d363
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2024-08-27 10:37:43 +0800

    feat: separate release for layout, ui, esm (#22)
    * feat: separate release for layout, ui, esm

---
+++ packages/rettangoli-ui/src/entry-esm.js
@@ -0,0 +1,13 @@
+import RettangoliButton from './components/button.js'
+import RettangoliView from './components/view.js';
+import RettangoliText from './components/text.js';
+import RettangoliImage from './components/image.js';
+import RettangoliSvg from './components/svg.js';
+
+export {
+    RettangoliButton,
+    RettangoliView,
+    RettangoliText,
+    RettangoliImage,
+    RettangoliSvg
+}
