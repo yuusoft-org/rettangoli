b0c673482d164d372802412f9d7764d16483538f
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-06-09 22:36:52 +0800

    fix rettangoli-ui build (#82)

--- packages/rettangoli-ui/src/entry-iife-layout.js
+++ packages/rettangoli-ui/src/entry-iife-layout.js
@@ -3,9 +3,13 @@ import RettangoliView from './primitives/view.js';
 import RettangoliText from './primitives/text.js';
 import RettangoliImage from './primitives/image.js';
 import RettangoliSvg from './primitives/svg.js';
+import RettangoliInput from './primitives/input.js';
+import RettangoliTextArea from './primitives/textarea.js';
 
 customElements.define("rtgl-button", RettangoliButton({}));
 customElements.define("rtgl-view", RettangoliView({}));
 customElements.define("rtgl-text", RettangoliText({}));
 customElements.define("rtgl-image", RettangoliImage({}));
 customElements.define("rtgl-svg", RettangoliSvg({}));
+customElements.define("rtgl-input", RettangoliInput({}));
+customElements.define("rtgl-textarea", RettangoliTextArea({}));
