4474594eb80239f8f07d895400d6b601a67075b5
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-06-11 11:32:18 +0800

    dropdown-menu implement click-item event

--- packages/rettangoli-ui/src/components/dropdownMenu/dropdownMenu.handlers.js
+++ packages/rettangoli-ui/src/components/dropdownMenu/dropdownMenu.handlers.js
@@ -3,3 +3,16 @@ export const handleClickPopoverOverlay = (e, deps) => {
   const { dispatchEvent } = deps;
   dispatchEvent(new CustomEvent('click-overlay'));
 }
+
+export const handleClickMenuItem = (e, deps) => {
+  const { dispatchEvent } = deps;
+  const index = parseInt(e.currentTarget.id.replace('option-', ''));
+  const item = deps.props.items[index];
+  
+  dispatchEvent(new CustomEvent('click-item', {
+    detail: {
+      index,
+      item
+    }
+  }));
+}
