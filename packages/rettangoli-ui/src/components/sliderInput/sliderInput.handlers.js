2e04eb655fbbd0601c033f0551dc261287e61316
738NGX <jny738ngx@gmail.com>
2025-07-18 21:47:56 +0800

    Slider input (#128)
    * init add
    
    * support input step
    
    * support form
    
    * report & accept
    
    * fix problems
    
    * report &b accept

---
+++ packages/rettangoli-ui/src/components/sliderInput/sliderInput.handlers.js
@@ -0,0 +1,24 @@
+export const handleOnMount = (deps) => {
+  const { store, attrs } = deps;
+  store.setValue(attrs.defaultValue || 0);
+} 
+
+export const handleValueChange = (e, deps) => {
+  const { store, render, dispatchEvent } = deps;
+  const newValue = Number(e.detail.value);
+
+  store.setValue(newValue);
+  
+  // Re-render to sync slider and input
+  render();
+  
+  // Dispatch event for external listeners
+  dispatchEvent(
+    new CustomEvent("slider-input-value-change", {
+      detail: {
+        value: newValue,
+      },
+      bubbles: true,
+    }),
+  );
+};
---
+++ packages/rettangoli-ui/src/components/sliderInput/sliderInput.store.js
@@ -0,0 +1,17 @@
+export const INITIAL_STATE = Object.freeze({
+  value: 0
+});
+
+export const toViewData = ({ state, attrs }) => {
+  return {
+    value: state.value,
+    w: attrs.w || '',
+    min: attrs.min || 0,
+    max: attrs.max || 100,
+    step: attrs.step || 1
+  };
+}
+
+export const setValue = (state, newValue) => {
+  state.value = newValue;
+}
