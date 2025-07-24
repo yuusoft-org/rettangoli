f01074e1793298b3a88db4e4dbb03ff04adcec64
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-07-24 23:02:26 +0800

    add breadcrumb component (#142)

---
+++ packages/rettangoli-ui/src/components/breadcrumb/breadcrumb.handlers.js
@@ -0,0 +1,9 @@
+export const handleClickItem = (e, deps) => {
+  const { dispatchEvent } = deps;
+  const id = e.currentTarget.dataset.id;
+  dispatchEvent(new CustomEvent('item-click', {
+    detail: {
+      id
+    }
+  }));
+}
--- packages/rettangoli-ui/vt/static/public/rtgl-icons.js
+++ packages/rettangoli-ui/vt/static/public/rtgl-icons.js
@@ -30,12 +30,15 @@ window.rtglIcons = {
 </svg>
   `,
   spinner: `                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="12" cy="12" r="10" 
-                      stroke="currentColor" 
-                      stroke-width="2" 
+              <circle cx="12" cy="12" r="10"
+                      stroke="currentColor"
+                      stroke-width="2"
                       fill="none"
                       stroke-dasharray="50 10"
                       stroke-linecap="round">
               </circle>
-          </svg>`
+          </svg>`,
+  'breadcrumb-arrow': `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`,
 }
