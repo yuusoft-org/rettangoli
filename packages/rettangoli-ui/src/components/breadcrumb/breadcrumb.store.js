636e30a04af940620589c623d1bfcfd157a0c5df
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-07-24 22:49:33 +0800

    add breadcrumb component

---
+++ packages/rettangoli-ui/src/components/breadcrumb/breadcrumb.store.js
@@ -0,0 +1,33 @@
+export const INITIAL_STATE = Object.freeze({});
+
+const blacklistedAttrs = ['id', 'class', 'style', 'slot'];
+
+const stringifyAttrs = (attrs) => {
+  return Object.entries(attrs).filter(([key]) => !blacklistedAttrs.includes(key)).map(([key, value]) => `${key}=${value}`).join(' ');
+}
+
+export const toViewData = ({ state, props, attrs }) => {
+  const containerAttrString = stringifyAttrs(attrs);
+  
+  const items = props.items || [];
+  const separator = props.separator || 'breadcrumb-arrow';
+
+  // Add separators between items, but not after the last one
+  const itemsWithSeparators = [];
+  items.forEach((item, index) => {
+    itemsWithSeparators.push(item);
+    if (index < items.length - 1) {
+      itemsWithSeparators.push({ isSeparator: true });
+    }
+  });
+
+  return {
+    containerAttrString,
+    items: itemsWithSeparators,
+    separator
+  };
+}
+
+export const setState = (state) => {
+  // do something
+}
--- packages/rettangoli-ui/vt/static/public/rtgl-icons.js
+++ packages/rettangoli-ui/vt/static/public/rtgl-icons.js
@@ -37,5 +37,11 @@ window.rtglIcons = {
                       stroke-dasharray="50 10"
                       stroke-linecap="round">
               </circle>
-          </svg>`
+          </svg>`,
+  'breadcrumb-arrow': `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`,
+  'chevron-right': `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.5 5L15.5 12L8.5 19" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`
 }
