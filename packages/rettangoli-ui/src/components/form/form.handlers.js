636c22ea91ebffe12467d3ed344777d75715a9ba
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-07-25 22:13:56 +0800

    update

--- packages/rettangoli-ui/src/components/form/form.handlers.js
+++ packages/rettangoli-ui/src/components/form/form.handlers.js
@@ -3,24 +3,6 @@ export const handleBeforeMount = (deps) => {
   store.setFormValues(props.defaultValues);
 };
 
-export const handleOnUpdate = (changes, deps) => {
-  const { oldProps, newProps } = changes;
-  const { render } = deps;
-  
-  console.log('handleOnUpdate called', {
-    oldFieldResources: oldProps.fieldResources,
-    newFieldResources: newProps.fieldResources,
-    hasChanged: oldProps.fieldResources !== newProps.fieldResources
-  });
-  
-  // Check if fieldResources has changed
-  if (oldProps.fieldResources !== newProps.fieldResources) {
-    console.log('fieldResources changed, triggering re-render');
-    // Trigger re-render to update image sources
-    render();
-  }
-};
-
 const dispatchFormChange = (name, fieldValue, formValues, dispatchEvent) => {
   dispatchEvent(
     new CustomEvent("form-change", {
