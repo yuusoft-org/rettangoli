30d7259cbc31e7a10b081f26c41cddfcf38bcb04
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-08-14 15:05:44 +0800

    fix rtgl-form select object default values

--- packages/rettangoli-ui/src/common.js
+++ packages/rettangoli-ui/src/common.js
@@ -180,6 +180,25 @@ function convertObjectToCssString(styleObject, selector = ':host') {
   return result;
 }
 
+// Deep equality helper for comparing values (including objects)
+export const deepEqual = (a, b) => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (typeof a !== 'object' || typeof b !== 'object') return false;
+  
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+  
+  if (keysA.length !== keysB.length) return false;
+  
+  for (const key of keysA) {
+    if (!keysB.includes(key)) return false;
+    if (!deepEqual(a[key], b[key])) return false;
+  }
+  
+  return true;
+};
+
 export {
   css,
   generateCSS,
