362a8c785e1ab71e9838501a554516b4ec0f3862
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-08-21 01:15:35 +0800

    implement form tooltips

--- packages/rettangoli-ui/vt/static/public/rtgl-icons.js
+++ packages/rettangoli-ui/vt/static/public/rtgl-icons.js
@@ -47,4 +47,8 @@ window.rtglIcons = {
   x: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`,
+  info: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2" fill="none"/>
+<path d="M12 16V12M12 8H12.01" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`,
 }
