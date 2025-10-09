5ace18f34aa5e70dea357bf9cc25dddcd33b21bd
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-10-09 16:00:14 +0800

    add min height width for scroll (#198)

--- packages/rettangoli-ui/src/styles/scrollStyles.js
+++ packages/rettangoli-ui/src/styles/scrollStyles.js
@@ -5,14 +5,18 @@ export default css`
 :host([sh]:not([sv])) {
     overflow-x: scroll;
     flex-wrap: nowrap;
+    min-width: 0;
 }
 :host([sv]:not([sh])) {
     overflow-y: scroll;
     flex-wrap: nowrap;
+    min-height: 0;
 }
 :host([sh][sv]) {
     overflow: scroll;
     flex-wrap: nowrap;
+    min-height: 0;
+    min-width: 0;
 }
 :host([overflow="hidden"]) {
     overflow: hidden;
