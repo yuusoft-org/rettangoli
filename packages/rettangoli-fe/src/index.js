7eec86976c62edd6dba425124753214721018555
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-12-08 06:36:44 +0100

    feat(fe): export build function

--- packages/rettangoli-fe/src/index.js
+++ packages/rettangoli-fe/src/index.js
@@ -1,7 +1,9 @@
 import createComponent from './createComponent.js';
 import createWebPatch from './createWebPatch.js';
+import { build } from './cli/index.js'
 
 export {
   createComponent,
   createWebPatch,
+  build
 }
