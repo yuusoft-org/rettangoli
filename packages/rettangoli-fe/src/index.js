8311831ec27f1c98cb49d7685412495614511ef4
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-12-09 01:22:04 +0100

    fix(fe) revert build export

--- packages/rettangoli-fe/src/index.js
+++ packages/rettangoli-fe/src/index.js
@@ -1,9 +1,7 @@
 import createComponent from './createComponent.js';
 import createWebPatch from './createWebPatch.js';
-import { build } from './cli/index.js'
 
 export {
   createComponent,
   createWebPatch,
-  build
 }
