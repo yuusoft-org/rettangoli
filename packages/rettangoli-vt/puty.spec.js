0d2aafdb466e9057877391d888f81b4b63769d04
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-02-09 15:14:07 +0800

    chore(vt): add vitest puty plugin, remove obsolete files
    - Add vitest.config.js to enable puty plugin for YAML spec tests
    - Remove docker-e2e.spec.js (superseded by e2e/run.js)
    - Remove puty.spec.js (orphaned, replaced by vitest plugin)
    - Remove docker-build-check.sh (redundant with docker-e2e-test.sh)
    - Clean up .npmignore

--- packages/rettangoli-vt/puty.spec.js
+++
@@ -1,6 +0,0 @@
-import path from "node:path";
-import { setupTestSuiteFromYaml } from "puty";
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-await setupTestSuiteFromYaml(__dirname);
