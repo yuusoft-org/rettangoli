b89884fe2e618093f679ed17a4a01a1a133ae153
han4wluc <han4wluc@yuusoft.com>
2026-04-06 18:02:06 +0800

    feat(ui): add segmented control component

---
+++ packages/rettangoli-ui/src/components/segmentedControl/segmentedControl.handlers.js
@@ -0,0 +1,172 @@
+import { deepEqual } from "../../common.js";
+
+const emitValueChange = ({ dispatchEvent, value, label, index, item }) => {
+  dispatchEvent(
+    new CustomEvent("value-change", {
+      detail: {
+        value,
+        label,
+        index,
+        item,
+      },
+      bubbles: true,
+    }),
+  );
+};
+
+export const handleBeforeMount = (deps) => {
+  const { store, props, render } = deps;
+
+  if (
+    props.selectedValue !== null
+    && props.selectedValue !== undefined
+    && props.options
+  ) {
+    const selectedOption = props.options.find((opt) =>
+      deepEqual(opt.value, props.selectedValue),
+    );
+    if (selectedOption) {
+      store.updateSelectedValue({
+        value: selectedOption.value,
+      });
+      render();
+    }
+  }
+};
+
+export const handleOnUpdate = (deps, payload) => {
+  const { oldProps, newProps } = payload;
+  const { store, render } = deps;
+  let shouldRender = false;
+
+  if (oldProps.selectedValue !== newProps.selectedValue) {
+    store.updateSelectedValue({ value: newProps.selectedValue });
+    shouldRender = true;
+  }
+
+  if (shouldRender) {
+    render();
+  }
+};
+
+export const handleOptionClick = (deps, payload) => {
+  const { render, dispatchEvent, props, store } = deps;
+  if (props.disabled) return;
+
+  const event = payload._event;
+  event.stopPropagation();
+
+  const id = event.currentTarget.id.slice("option".length);
+  const index = Number(id);
+  const option = props.options[index];
+  const hasControlledValue = Object.prototype.hasOwnProperty.call(
+    props || {},
+    "selectedValue",
+  );
+  const currentValue = store.selectSelectedValue();
+  const hasCurrentValue = hasControlledValue ? true : store.selectHasSelectedValue();
+  const isSelected = option
+    ? hasCurrentValue && deepEqual(option.value, currentValue)
+    : false;
+
+  if (!option) {
+    return;
+  }
+
+  if (isSelected && !props.noClear) {
+    store.clearSelectedValue({});
+
+    if (props.onChange && typeof props.onChange === "function") {
+      props.onChange(undefined);
+    }
+
+    emitValueChange({
+      dispatchEvent,
+      value: undefined,
+      label: undefined,
+      index: null,
+      item: undefined,
+    });
+
+    render();
+    return;
+  }
+
+  store.updateSelectedValue({ value: option.value });
+
+  if (props.onChange && typeof props.onChange === "function") {
+    props.onChange(option.value);
+  }
+
+  emitValueChange({
+    dispatchEvent,
+    value: option.value,
+    label: option.label,
+    index,
+    item: option,
+  });
+
+  render();
+};
+
+export const handleOptionKeyDown = (deps, payload) => {
+  const event = payload._event;
+  if (event.key !== "Enter" && event.key !== " ") {
+    return;
+  }
+
+  event.preventDefault();
+  handleOptionClick(deps, payload);
+};
+
+export const handleOptionMouseEnter = (deps, payload) => {
+  const { store, render } = deps;
+  const event = payload._event;
+  const id = parseInt(event.currentTarget.id.slice("option".length), 10);
+  store.setHoveredOption({ optionId: id });
+  render();
+};
+
+export const handleOptionMouseLeave = (deps) => {
+  const { store, render } = deps;
+  store.clearHoveredOption({});
+  render();
+};
+
+export const handleAddOptionClick = (deps, payload) => {
+  if (deps.props.disabled) return;
+
+  const { render, dispatchEvent } = deps;
+  const { _event: event } = payload;
+  event.stopPropagation();
+
+  dispatchEvent(
+    new CustomEvent("add-option-click", {
+      bubbles: true,
+    }),
+  );
+
+  render();
+};
+
+export const handleAddOptionKeyDown = (deps, payload) => {
+  const event = payload._event;
+  if (event.key !== "Enter" && event.key !== " ") {
+    return;
+  }
+
+  event.preventDefault();
+  handleAddOptionClick(deps, payload);
+};
+
+export const handleAddOptionMouseEnter = (deps) => {
+  const { store, render } = deps;
+  store.setHoveredAddOption({ isHovered: true });
+  render();
+};
+
+export const handleAddOptionMouseLeave = (deps) => {
+  const { store, render } = deps;
+  store.setHoveredAddOption({ isHovered: false });
+  render();
+};
---
+++ packages/rettangoli-ui/src/components/segmentedControl/segmentedControl.store.js
@@ -0,0 +1,104 @@
+import { deepEqual } from "../../common.js";
+
+const blacklistedProps = [
+  "id",
+  "class",
+  "style",
+  "slot",
+  "placeholder",
+  "selectedValue",
+  "onChange",
+  "options",
+  "noClear",
+  "addOption",
+  "disabled",
+];
+
+const stringifyProps = (props = {}) => {
+  return Object.entries(props)
+    .filter(([key]) => !blacklistedProps.includes(key))
+    .map(([key, value]) => `${key}=${value}`)
+    .join(" ");
+};
+
+export const createInitialState = () =>
+  Object.freeze({
+    selectedValue: null,
+    hasSelectedValue: false,
+    hoveredOptionId: null,
+    hoveredAddOption: false,
+  });
+
+export const selectViewData = ({ state, props }) => {
+  const containerAttrString = stringifyProps(props);
+  const isDisabled = !!props.disabled;
+  const hasControlledValue = Object.prototype.hasOwnProperty.call(
+    props || {},
+    "selectedValue",
+  );
+  const currentValue = hasControlledValue ? props.selectedValue : state.selectedValue;
+  const hasCurrentValue = hasControlledValue ? true : !!state.hasSelectedValue;
+  const options = props.options || [];
+
+  const optionsWithSelection = options.map((option, index) => {
+    const isSelected = hasCurrentValue && deepEqual(option.value, currentValue);
+    const isHovered = state.hoveredOptionId === index;
+
+    return {
+      ...option,
+      isSelected,
+      bgc: isSelected ? "ac" : (isHovered && !isDisabled ? "mu" : ""),
+      textColor: isSelected ? "ac-fg" : "fg",
+      borderLeftWidth: index === 0 ? "none" : "xs",
+      cursor: isDisabled ? "not-allowed" : "pointer",
+      tabIndex: isDisabled ? -1 : 0,
+    };
+  });
+
+  return {
+    containerAttrString,
+    isDisabled,
+    options: optionsWithSelection,
+    selectedValue: currentValue,
+    hasSelectedValue: hasCurrentValue,
+    ariaLabel: props.placeholder || "Segmented control",
+    showAddOption: !isDisabled && !!props.addOption,
+    addOptionLabel: props.addOption?.label ? `+ ${props.addOption.label}` : "+ Add",
+    addOptionBgc: state.hoveredAddOption ? "mu" : "",
+    addOptionBorderLeftWidth: options.length === 0 ? "none" : "xs",
+  };
+};
+
+export const selectState = ({ state }) => {
+  return state;
+};
+
+export const selectSelectedValue = ({ state }) => {
+  return state.selectedValue;
+};
+
+export const selectHasSelectedValue = ({ state }) => {
+  return !!state.hasSelectedValue;
+};
+
+export const updateSelectedValue = ({ state }, payload = {}) => {
+  state.selectedValue = payload.value;
+  state.hasSelectedValue = true;
+};
+
+export const clearSelectedValue = ({ state }) => {
+  state.selectedValue = undefined;
+  state.hasSelectedValue = false;
+};
+
+export const setHoveredOption = ({ state }, payload = {}) => {
+  state.hoveredOptionId = payload.optionId;
+};
+
+export const clearHoveredOption = ({ state }) => {
+  state.hoveredOptionId = null;
+};
+
+export const setHoveredAddOption = ({ state }, payload = {}) => {
+  state.hoveredAddOption = !!payload.isHovered;
+};
