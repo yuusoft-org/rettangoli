c353f1dd2190045d328b61adc95ee5a1b98189c5
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-04-22 19:50:01 +0800

    fix(tag-select): preserve open draft state on option updates (#409)
    * fix(tag-select): preserve open draft state on option updates
    
    * fix(tag-select): preserve legacy store view data fields
    
    * refactor(tag-select): drop legacy store view data aliases

--- packages/rettangoli-ui/src/components/tagSelect/tagSelect.methods.js
+++ packages/rettangoli-ui/src/components/tagSelect/tagSelect.methods.js
@@ -1,3 +1,5 @@
+import { deepEqual } from "../../common.js";
+
 const resolvePopoverPosition = (trigger) => {
   if (!trigger || typeof trigger.getBoundingClientRect !== "function") {
     return null;
@@ -11,12 +13,66 @@ const resolvePopoverPosition = (trigger) => {
   };
 };
 
-export const refreshPopover = function () {
+const normalizeSelectedValues = (selectedValues) => {
+  if (!Array.isArray(selectedValues)) {
+    return [];
+  }
+
+  return [...selectedValues];
+};
+
+const renderInstance = (instance) => {
+  if (typeof instance.render === "function") {
+    instance.render();
+  }
+};
+
+const resolveCurrentValues = (instance) => {
+  if (instance.store?.selectHasSelectedValues?.()) {
+    return instance.store?.selectSelectedValues?.() || [];
+  }
+
+  return normalizeSelectedValues(instance.props?.selectedValues);
+};
+
+const openPopoverWithDraft = (instance, values = []) => {
+  const position = resolvePopoverPosition(instance.refs?.trigger);
+
+  if (!position || !instance.store?.openOptionsPopover) {
+    return false;
+  }
+
+  instance.store.openOptionsPopover({
+    position,
+    values,
+  });
+
+  return true;
+};
+
+const setDraftValues = (instance, values = [], keepOpen = false) => {
+  const state = instance.store?.getState?.();
+
+  if (state?.isOpen && instance.store?.updateDraftSelectedValues) {
+    instance.store.updateDraftSelectedValues({ values });
+    return true;
+  }
+
+  if (keepOpen) {
+    return openPopoverWithDraft(instance, values);
+  }
+
+  return false;
+};
+
+export const refreshPopover = function (payload = {}) {
   const state = this.store?.getState?.();
+  const draftValues = Array.isArray(payload.values)
+    ? normalizeSelectedValues(payload.values)
+    : (this.store?.selectDraftSelectedValues?.() || []);
 
   if (state?.isOpen) {
     const position = resolvePopoverPosition(this.refs?.trigger);
-    const draftValues = this.store?.selectDraftSelectedValues?.() || [];
 
     if (position) {
       this.store.openOptionsPopover({
@@ -24,9 +80,46 @@ export const refreshPopover = function () {
         values: draftValues,
       });
     }
+  } else if (payload.keepOpen) {
+    openPopoverWithDraft(this, draftValues.length > 0 ? draftValues : resolveCurrentValues(this));
   }
 
-  if (typeof this.render === "function") {
-    this.render();
+  renderInstance(this);
+};
+
+export const setDraftSelectedValues = function (payload = {}) {
+  const values = normalizeSelectedValues(payload.values);
+
+  if (!setDraftValues(this, values, !!payload.keepOpen)) {
+    return;
   }
+
+  renderInstance(this);
+};
+
+export const appendDraftSelectedValue = function (payload = {}) {
+  if (!Object.prototype.hasOwnProperty.call(payload || {}, "value")) {
+    return;
+  }
+
+  const state = this.store?.getState?.();
+  const currentValues = state?.isOpen
+    ? (this.store?.selectDraftSelectedValues?.() || [])
+    : resolveCurrentValues(this);
+
+  if (currentValues.some((currentValue) => deepEqual(currentValue, payload.value))) {
+    if (!!payload.keepOpen && !state?.isOpen && openPopoverWithDraft(this, currentValues)) {
+      renderInstance(this);
+    }
+
+    return;
+  }
+
+  const nextValues = [...normalizeSelectedValues(currentValues), payload.value];
+
+  if (!setDraftValues(this, nextValues, !!payload.keepOpen)) {
+    return;
+  }
+
+  renderInstance(this);
 };
