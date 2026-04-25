e33253363612e1ca45d068619d1fc181d2ed7299
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-04-26 00:50:02 +0800

    Fix popover repositioning after content changes (#414)

---
+++ packages/rettangoli-ui/src/common/popover.js
@@ -0,0 +1,74 @@
+export const calculatePopoverPosition = ({
+  x,
+  y,
+  width,
+  height,
+  place,
+  viewportWidth,
+  viewportHeight,
+  offset = 8,
+  padding = 8,
+}) => {
+  let left = x;
+  let top = y;
+
+  switch (place) {
+    case "t":
+      left = x - width / 2;
+      top = y - height - offset;
+      break;
+    case "ts":
+      left = x;
+      top = y - height - offset;
+      break;
+    case "te":
+      left = x - width;
+      top = y - height - offset;
+      break;
+    case "r":
+      left = x + offset;
+      top = y - height / 2;
+      break;
+    case "rs":
+      left = x + offset;
+      top = y;
+      break;
+    case "re":
+      left = x + offset;
+      top = y - height;
+      break;
+    case "b":
+      left = x - width / 2;
+      top = y + offset;
+      break;
+    case "bs":
+      left = x;
+      top = y + offset;
+      break;
+    case "be":
+      left = x - width;
+      top = y + offset;
+      break;
+    case "l":
+      left = x - width - offset;
+      top = y - height / 2;
+      break;
+    case "ls":
+      left = x - width - offset;
+      top = y;
+      break;
+    case "le":
+      left = x - width - offset;
+      top = y - height;
+      break;
+    default:
+      left = x;
+      top = y + offset;
+      break;
+  }
+
+  return {
+    left: Math.max(padding, Math.min(left, viewportWidth - width - padding)),
+    top: Math.max(padding, Math.min(top, viewportHeight - height - padding)),
+  };
+};
